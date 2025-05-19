1620be6ee0831755b7ffc489afd50f42ae0caf27
unknown <z.abbas83@yahoo.com>
2025-05-19 21:19:36 +0200

    Etape 2 JS est faite!!

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -3,3 +3,10 @@ let avatar = document.getElementById("avatar");
 avatar.addEventListener("click", () => {
     avatar.src = "./image/avatar-bis.png"
 })
+
+let firstname = document.getElementById("firstname");
+
+firstname.addEventListener("click", () => {
+    firstname.textContent = "Zaher";
+    firstname.style.color = "white";
+})
