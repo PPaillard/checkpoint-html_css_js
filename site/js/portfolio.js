807e3532a7a5b422a99140b25d02b79c701976b5
unknown <z.abbas83@yahoo.com>
2025-05-19 22:00:27 +0200

    Etape 5 JS changer la couleur de tout les liens a est faite!

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -11,7 +11,19 @@ firstname.addEventListener("click", () => {
     firstname.style.color = "white";
 })
 
-document.body.addEventListener("dblclick", () => {
-    let couleur = prompt("Veuillez selectionner une couleur de fond:");
-    document.body.style.backgroundColor = couleur;
+let btnFond = document.getElementById("btnFond");
+
+btnFond.addEventListener("click", () => {
+    let couleur1 = prompt("Veuillez selectionner une couleur de fond:");
+    document.body.style.backgroundColor = couleur1;
+})
+
+let btnLiens = document.getElementById("btnLiens");
+
+btnLiens.addEventListener("click", () => {
+    let couleur2 = prompt("Veuillez selectionner une couleur pour les liens HTML:");
+    let liens = document.getElementsByTagName("a");
+    for (const lien of liens) {
+        lien.style.color = couleur2;
+    }
 })
