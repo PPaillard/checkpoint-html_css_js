a6f2ea01a398c20e76a985446e7fd4e46e6edd3a
BBoezio <benjamin.boezio@labom2iformation.fr>
2025-05-19 16:09:31 +0200

    Finish algo 3 in part 1

--- algo/3/findSeniors.js
+++ algo/3/findSeniors.js
@@ -25,8 +25,49 @@ const persons = [
 
 */
 
+const persons = [
+  { name: 'Mary', experience: 2, job: 'web dev' },
+  { name: 'Tony', experience: 6, job: 'data analyst' },
+  { name: 'John', experience: 2, job: 'data analyst' },
+  { name: 'Jane', experience: 6, job: 'web dev' },
+  { name: 'Maggie', experience: 2, job: 'web dev' },
+  { name: 'Leonardo', experience: 2, job: 'data analyst' },
+  { name: 'Carla', experience: 4, job: 'data analyst' },
+  { name: 'Mickael', experience: 7, job: 'web dev' },
+  { name: 'Penelope', experience: 7, job: 'web dev' },
+  { name: 'Homer', experience: 5, job: 'data analyst' },
+  { name: 'Leonardo', experience: 2, job: 'data analyst' },
+  { name: 'Carla', experience: 4, job: 'web dev' },
+  { name: 'Lisa', experience: 3, job: 'web dev' },
+  { name: 'Millie', experience: 5, job: 'data analyst' },
+  { name: 'Penelope', experience: 7, job: 'web dev' },
+];
+
 function findSeniors(persons) {
-  // Your code here !
+  const dev = [],
+        dataAnalyst = [];
+
+  let i = 0, j = 0;
+
+  for( const person of persons ){
+
+    if( person.experience >= 5 ){
+      if( person.job == "web dev" ){
+        
+        i = dev.length;
+        dev[i] = { name: person.name, experience: person.experience };
+
+      }else if( person.job == "data analyst" ){
+        
+        j = dataAnalyst.length;
+        dataAnalyst[j] = { name: person.name,  experience: person.experience };
+
+      }
+    }
+
+  }
+  
+  return [dev, dataAnalyst];
 }
 
 module.exports = findSeniors;
