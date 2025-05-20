446ecbafe30b5b8e59aee4338c11433ce4257a3c
PierreP <paillard.pierre@gmail.com>
2025-05-20 14:43:56 +0200

    1er algo opé

--- algo/1/thirdAngle.js
+++ algo/1/thirdAngle.js
@@ -7,8 +7,9 @@ thirdAngle(20, 80) doit retourner 80
 */
 
 function thirdAngle(a, b) {
-  // Your code here !
-  return 4;
+  // a + b + c = 180
+  // c = 180 -a - b
+  return 180 - a - b;
 }
 
 module.exports = thirdAngle;
