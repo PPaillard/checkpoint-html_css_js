e8ce21c07488404ae4a8d5b3dbd8c0bc0003bd0a
Justclara42 <clara.holderbaum@icloud.com>
2025-05-20 14:33:37 +0200

    bonus fait jusqu'à étape 4

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -2,5 +2,53 @@ function changeName() {
   let person = prompt("Merci d'entrer votre nom");
   if (person != null) {
     document.getElementById("firstname").textContent = person ;
+    document.getElementById("firstname").style.color = "white" ;
   }
 }
+
+function changeAvatar() {
+    const avatar = document.getElementById("avatar");
+
+    avatar.addEventListener("click", () => {
+        if (avatar.src.endsWith("avatar.svg")) {
+            avatar.src = "./image/avatar-bis.png";
+        } else {
+            avatar.src = "./image/avatar.svg";
+        }
+    });
+    
+}
+
+    const bgColor = document.querySelector(".description");
+    const changeBg = document.getElementById("changeBg");
+
+    changeBg.addEventListener("click", () => {
+        bgColor.style.backgroundColor = "#750ff7";
+
+    });
+
+
+    const textPink = document.getElementsByClassName("pink-text");
+    const changeColor = document.getElementById("changeColor");
+
+    changeColor.addEventListener("click", ()=>{
+        
+        for(let i = 0; i < textPink.length; i++){
+            textPink[i].style.color = "#750ff7";
+        }
+    });
+
+
+const changeLi = document.getElementById("changeLi");
+const list = document.querySelector("#front-dev-tools");
+const listLi = list.querySelectorAll("li");
+const newList = ["VScode", "Photoshop", "Laravel", "MacOS"];
+
+changeLi.addEventListener("click", () => {
+    for(let i = 0; i < listLi.length; i++){
+        for(let j = 0; j < newList.length; j++){
+            
+            listLi[i].textContent = newList[j++]
+        }
+    }
+})
