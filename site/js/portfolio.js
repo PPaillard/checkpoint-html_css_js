38aefd42e33fa1dc8a4482f000fbb3a3127cc2e4
shalom-gnassounou <shalom.gnassounou@labom2iformation.fr>
2025-05-20 15:32:05 +0200

    Etape 4 et 5 de JS

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -15,20 +15,35 @@ changeName.addEventListener("click", () => {
 });
 
 // je selectionne les elems rose
-const bgColors = document.querySelectorAll("pink-bg");
+const bgColors = document.querySelectorAll(".pink-bg");
 // je selectionne mon bouton pour changer la couleur
 const changeColor = document.getElementById("changeColor");
-// 
+//
 
-  changeColor.addEventListener("click", () => {
-    for (const bgColor of bgColors) {
-     bgColor.style.backgroundColor = "#750ff7";
-     }
-  });
-
+changeColor.addEventListener("click", () => {
+  for (const bgColor of bgColors) {
+    bgColor.style.backgroundColor = "#750ff7";
+  }
+});
 
 // Je dois selectionner un element
 // je dois selectionner les elements ayant une couleur de fond rose
 // qund je click sur cet element
 // Pour chaque element qui a une couleur de fond rose dans ma liste d'elements ayant une couleur de fond rose.
 // je change sa couleur de fond.
+
+const changeColorElements = document.getElementById("changeColorElements"); //button
+const pinkElements = document.querySelectorAll(".pink-text"); // Elements rose ?
+
+changeColorElements.addEventListener("click", () => {
+  for (const pinkElement of pinkElements) {
+    const yourColor = prompt("Votre couleur change");
+    pinkElement.style.color = `${yourColor}`;
+  }
+});
+
+const colorLinks = document.querySelectorAll("a");
+for (const colorLink of colorLinks) {
+  colorLink.style.color = "#750ff7";
+}
+
