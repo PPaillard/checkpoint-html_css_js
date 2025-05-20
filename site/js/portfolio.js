ece6ba3b8412d00127591332d8097b4f41b89776
chloe974 <chloe.previl@gmail.com>
2025-05-20 11:51:15 +0200

    Étape 1 JS

---
+++ site/js/portfolio.js
@@ -0,0 +1,8 @@
+const ChangeAvatarId = document.getElementById("img-avatar");
+
+ChangeAvatarId.addEventListener ("click", ()=>{
+    ChangeAvatarId.setAttribute("src", "image/avatar.svg")
+}
+)
+
+
