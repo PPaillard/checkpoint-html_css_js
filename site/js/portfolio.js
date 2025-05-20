4f55e7deba37e28d1906a5c518f474bf76e599d7
Ethan B <e.boulard07@gmail.com>
2025-05-20 14:28:21 +0200

    save change

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -22,14 +22,61 @@ btn.addEventListener('click', () => {
 
 // Exo 3
 const changeColor = document.querySelector('#changeColor')
-const pink = 
-
+const pink = document.getElementsByClassName("pink-bg")
 changeColor.addEventListener('click', () => {
-    document.getElementsByClassName("description pink-bg").classList.add("changeColors")
-    //pink.style.backgroundColor = "#750ff7"
-  }
+    for(const color of pink){
+        color.classList.add("changeColors")
+    }
+    //pink. = "#750ff7"
+}
 )
-//????????????
 
 // Exo 4 
-const allPink = document.querySelector('allPink')
+const myColor = document.querySelector('#myColor')
+
+myColor.addEventListener('click', () => {
+    let mycolor = prompt("Please enter your une couleur:")
+     if (mycolor === null) {
+    log.innerText = "OK, maybe next time.";
+  } else {
+    for(const color of pink){
+    color.style.backgroundColor= `${mycolor}`
+    }
+} }
+)
+/*
+myColor.addEventListener("change", () => {
+        for(const color of pink){
+         color.style.backgroundColor("myColor.value")
+    }
+  }
+  
+)*/
+
+// Exo 5 
+
+const link = document.querySelectorAll('a')
+const colorLink = document.querySelector('#colorLink')
+
+colorLink.addEventListener('click', () => {
+    for(const color of link){
+        color.style.color= "red"
+    }
+}
+)
+
+// Exo 6
+
+
+const btnFront = document.querySelectorAll("changeFront")
+let frontList = [];
+i = 0
+j = 0
+
+btnFront.addEventListener('click', changeList)
+
+function changeList() {
+const front = document.querySelectorAll("front-dev-tools")    
+    for(const li of front){
+    }
+} 
