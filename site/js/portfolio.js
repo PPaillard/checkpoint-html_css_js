53a363d4cfd4ce08b693b7b2e8deca7b327f4b8a
chloe974 <chloe.previl@gmail.com>
2025-05-20 16:40:13 +0200

    Ajout jusqu'à l'étape 3 JS

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,3 +1,4 @@
+// Clique pour changer l'image de l'avatar
 const ChangeAvatarId = document.getElementById("img-avatar");
 
 ChangeAvatarId.addEventListener ("click", ()=>{
@@ -5,4 +6,22 @@ ChangeAvatarId.addEventListener ("click", ()=>{
 }
 )
 
+const ChangeNameID = document.getElementById("firstname");
+const ChangeNameButton = document.querySelector("#changerlenom")
 
+
+
+ChangeNameButton.addEventListener("click", (ChangeName)=>{
+    const ChangeNamePrompt = prompt("Quel nom souhaitez-vous mettre ?")
+    ChangeNameID.textContent=ChangeNamePrompt;
+    ChangeNameID.style.color = "white";
+}
+)
+
+document.getElementById("changerlefond").addEventListener("click", (ChangeColor)=>{
+    let color=prompt("Quelle couleur doit avoir votre fond ?");
+    const pinkBgs= document.getElementsByClassName("pink-bg");
+    for (const pinkBg of pinkBgs){
+        pinkBg.style.backgroundColor = color;
+    }
+})
