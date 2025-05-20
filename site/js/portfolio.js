39f4f8e1fdcd377d618a85d832999a8880b2e968
shalom-gnassounou <shalom.gnassounou@labom2iformation.fr>
2025-05-20 14:20:19 +0200

    Etape 3 de JavaScript

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -4,14 +4,31 @@ avatar.addEventListener("click", () => {
 });
 
 const firstName = document.getElementById("firstname");
-firstName.addEventListener("click",()=> {
-    firstName.style.color = "white"
-    
-})
+firstName.addEventListener("click", () => {
+  firstName.style.color = "white";
+});
 
-const changeName = document.getElementById("changeName")
-changeName.addEventListener("click",()=> {
-  const betterName = prompt("Votre nom ?")
-  firstName.textContent = (`${betterName}`)
-})
+const changeName = document.getElementById("changeName");
+changeName.addEventListener("click", () => {
+  const betterName = prompt("Votre nom ?");
+  firstName.textContent = `${betterName}`;
+});
 
+// je selectionne les elems rose
+const bgColors = document.querySelectorAll("pink-bg");
+// je selectionne mon bouton pour changer la couleur
+const changeColor = document.getElementById("changeColor");
+// 
+
+  changeColor.addEventListener("click", () => {
+    for (const bgColor of bgColors) {
+     bgColor.style.backgroundColor = "#750ff7";
+     }
+  });
+
+
+// Je dois selectionner un element
+// je dois selectionner les elements ayant une couleur de fond rose
+// qund je click sur cet element
+// Pour chaque element qui a une couleur de fond rose dans ma liste d'elements ayant une couleur de fond rose.
+// je change sa couleur de fond.
